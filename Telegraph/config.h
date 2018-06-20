8460ae1a90e0fe0ed8f7f04b0e6f78df40d261a3
夏亚峰 <xiayafeng@huiyoujia.com>
2018-06-20 23:55:28 +0800

    m

---
+++ Telegraph/config.h
@@ -0,0 +1,16 @@
+//
+//  config.h
+//  Telegraph
+//
+//  Created by yfxiari on 2018/6/12.
+//
+
+#ifndef config_h
+#define config_h
+
+#define SETUP_API_ID(apiId) apiId = 246590;
+
+#define SETUP_API_HASH(apiHash) apiHash= @"28a7718cbcf9919cf117bce82d989d89";
+
+
+#endif /* config_h */
